ed66c9d5f31868cf1dfee9790ca452c36a2cb812
Gal Hillel <Galh2011@gmail.com>
2024-05-24 18:46:53 +0300

    Add all the operators needed

--- Graph.hpp
+++ Graph.hpp
@@ -1,9 +1,8 @@
-// Id: 211696521 Mail: galh2011@icloud.com
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
 namespace ariel
 {
@@ -16,7 +15,34 @@ namespace ariel
         void printGraph() const;
         bool isValidGraph(const std::vector<std::vector<int>> &graph) const;
         int getVertices() const { return vertices; }
+        std::string toString() const;
         const std::vector<std::vector<int>> &getAdjacencyMatrix() const { return adjacencyMatrix; }
+
+        Graph &operator+=(const Graph &other);
+        Graph &operator-=(const Graph &other);
+        Graph operator+(const Graph &other) const;
+        Graph operator-(const Graph &other) const;
+        Graph operator+() const;
+        Graph operator-() const;
+        Graph &operator*=(int scalar);
+        Graph operator/(int scalar) const;
+        Graph &operator/=(int scalar);
+        Graph operator*(int scalar) const;
+        Graph operator*(const Graph &other) const;
+
+        bool operator==(const Graph &other) const;
+        bool operator!=(const Graph &other) const;
+        bool operator>(const Graph &other) const;
+        bool operator<(const Graph &other) const;
+        bool operator>=(const Graph &other) const;
+        bool operator<=(const Graph &other) const;
+
+        Graph &operator++();   // Prefix increment
+        Graph operator++(int); // Postfix increment
+        Graph &operator--();   // Prefix decrement
+        Graph operator--(int); // Postfix decrement
+
+        friend std::ostream &operator<<(std::ostream &os, const Graph &g);
 
     private:
         std::vector<std::vector<int>> adjacencyMatrix;
@@ -25,4 +51,4 @@ namespace ariel
 
 }
 
-#endif
+#endif // GRAPH_HPP
